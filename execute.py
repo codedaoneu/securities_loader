4e4cf750297895d671dd41db9fc55a578db2fc5e
codedaoneu <thang.nv0912@gmail.com>
2022-09-13 00:24:10 +0700

    add comment to test file

--- execute.py
+++ execute.py
@@ -2,5 +2,6 @@ from loader import StockLoader
 
 if __name__ == "__main__":
     VCB = StockLoader('2022-01-09', '2022-09-09')
+    # test function get data of a specific stock symbol
     data = VCB.load_data_of_a_symbol('VCB')
     print(data)
